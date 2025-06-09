3e21dde4f714b5200f249947ca7ba2355b6c8a3b
Manuele Vancin <mfvancin@gmail.com>
2025-06-09 13:52:46 +0100

    str

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(str(settings.DATABASE_URL))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
